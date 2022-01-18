89967256885dfc424ae43ee97d537200976fba23
Ivan <example.@gmail.com>
2022-01-18 06:44:09 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,110 @@
+// "use strict";
+// let number = 4.6;
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+
+// const person = 'Alex';
+
+// const bool = false;
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+
+// };
+// console.log(obj.name);
+// console.log(obj['name']);
+
+
+// let arr = ['plum.png', 'orang.png', 6, 'apple.bmp', {}, []];
+// console.log(arr['1 ']);
+
+// "use strict";
+
+// alert ('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', "");
+// console.log(answer + 5);
+
+
+// const answer = +prompt('Вам есть 18?', "");
+// console.log(answer + 5);
+
+// const answer = [];
+
+// answer[0] = prompt('Как ваше имя?', '');
+// answer[1] = prompt('Как ваше фамилия?', '');
+// answer[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answer));
+// console.log(typeof(null));
+
+
+
+
+
+
+
+// "use strict";
+
+// const category = 'toys';
+
+// // console.log('http//someurl.com' + category + '/' + '4');
+// console.log(`http//someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Privet! ${user}`);
+
+
+
+"use strict";
+
+console.log(4 + "-object");
+console.log(4 + +'5');
+
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(++incr);
+// console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+console.log(2*4 == '8');
+console.log(2*4 === '8');
+
+
+// && - "И"
+// || - "Или"
+// !isClose - "Отрицание false становиться true"
+
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked && isClose);
+
+
+
+
+
+
+
+
+
+
+
+
